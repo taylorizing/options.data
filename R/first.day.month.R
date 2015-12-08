ddf78b345ffc6131b0ba5f03bab1357a4c03085d
Jason Taylor <jason@themechanicalbear.com>
2015-12-08 07:47:05 -0700

    updating data files

--- R/first.day.month.R
+++ R/first.day.month.R
@@ -1,10 +1,70 @@
-# Read in the first day of the month for opening trade dates
+# title: "open.first.day.month.R"
+# author: "Jason Taylor"
 
-# Read in the csv that has all the first trading days of the month
-# Make data frame so that it can be used as entry dates
-first.day.file <- "first.day.month.csv"
-first.day.month <- fread(first.day.file)
-first.day.month <- as.data.frame(first.day.month)
-colnames(first.day.month) <- c("row.num", "open.date")
-first.day.month[, "open.date"] <- as.Date(first.day.month[, "open.date"])
-save(first.day.month, file = "first.day.month.Rdata")
+# Todos:
+# - None at this time
+
+# Description:
+# - Use this script to recreate first day month file for trade entry
+
+# Instructions:
+# - Update the market closed dates prior to running
+# - Calendar is created through 2020 update if needed
+
+# Ensure environment includes libraries needed
+library(dplyr)
+
+# Dates the market was closed and can'te be used in studies
+closed.dates <- c("2010-01-01", "2010-01-18","2010-02-15", "2010-04-02",
+                  "2010-05-31", "2010-07-05", "2010-09-06", "2010-11-25",
+                  "2010-12-24", "2011-01-17", "2011-02-21", "2011-04-22",
+                  "2011-05-30", "2011-07-04", "2011-09-05", "2011-11-24",
+                  "2011-12-26", "2012-01-02", "2012-01-16", "2012-02-20",
+                  "2012-04-06", "2012-05-28", "2012-07-04", "2012-09-03",
+                  "2012-11-22", "2012-12-25", "2013-01-01", "2013-01-21",
+                  "2013-02-18", "2013-03-29", "2013-05-27", "2013-07-04",
+                  "2013-09-02", "2013-11-28", "2013-12-25", "2014-01-01",
+                  "2014-01-20", "2014-02-17", "2014-04-18", "2014-05-26",
+                  "2014-07-04", "2014-09-11", "2014-11-27", "2014-12-25",
+                  "2015-01-19", "2015-02-16", "2015-05-25", "2015-07-03",
+                  "2015-09-07", "2015-11-26")
+closed.dates <- as.Date(closed.dates)
+
+# Create first day of the month data file for use on trade entry
+open.first.day.month <- data.frame(day = 1,
+                       mon = c(1:12, 1:12, 1:12, 1:12, 1:12, 1:12, 1:12, 1:12,
+                               1:12, 1:12, 1:12),
+                       year = c(rep(2010, 12),rep(2011, 12), rep(2012, 12),
+                                rep(2013, 12), rep(2014, 12), rep(2015, 12),
+                                rep(2016, 12), rep(2017, 12), rep(2018, 12),
+                                rep(2019, 12), rep(2020, 12)))
+# Combine the columns to create data frame of dates
+open.first.day.month <- dplyr::mutate(open.first.day.month,
+                                 date = as.Date(mdy(paste0(mon, "-", day,
+                                                           "-", year))))
+
+# If date is a market closed date move to next day
+open.first.day.month <- dplyr::mutate(open.first.day.month,
+                                 date = ifelse(date %in% closed.dates,
+                                               date + 1, date))
+
+# Find day of week for each date
+open.first.day.month <- dplyr::mutate(open.first.day.month,
+                                 day.week = weekdays(open.first.day.month$date,
+                                                     abbreviate = FALSE))
+# If date is Sat or Sun move to Monday
+open.first.day.month <- dplyr::mutate(open.first.day.month,
+                                 date = ifelse(day.week == "Saturday",
+                                               date + 2, date))
+open.first.day.month <- dplyr::mutate(open.first.day.month,
+                                 date = ifelse(day.week == "Sunday",
+                                               date + 1, date))
+open.first.day.month <- dplyr::select(open.first.day.month, date)
+open.first.day.month <- dplyr::mutate(open.first.day.month,
+                                 date = as.Date(date, origin = "1970-01-01"))
+
+# Save data to be resused in options.data package
+save(open.first.day.month, file = "data/open.first.day.month.RData")
+
+
+
---
+++ R/first.day.week.R
@@ -0,0 +1,104 @@
+# title: "open.first.day.week.R"
+# author: "Jason Taylor"
+
+# Todos:
+# - None at this time
+
+# Description:
+# - Use this script to recreate first day week file for trade entry
+
+# Instructions:
+# - Update the market closed dates prior to running
+# - Calendar is created through 2020 update if needed
+
+# Ensure environment includes libraries needed
+library(dplyr)
+
+# Dates the market was closed and can'te be used in studies
+closed.dates <- c("2010-01-01", "2010-01-18","2010-02-15", "2010-04-02",
+                  "2010-05-31", "2010-07-05", "2010-09-06", "2010-11-25",
+                  "2010-12-24", "2011-01-17", "2011-02-21", "2011-04-22",
+                  "2011-05-30", "2011-07-04", "2011-09-05", "2011-11-24",
+                  "2011-12-26", "2012-01-02", "2012-01-16", "2012-02-20",
+                  "2012-04-06", "2012-05-28", "2012-07-04", "2012-09-03",
+                  "2012-11-22", "2012-12-25", "2013-01-01", "2013-01-21",
+                  "2013-02-18", "2013-03-29", "2013-05-27", "2013-07-04",
+                  "2013-09-02", "2013-11-28", "2013-12-25", "2014-01-01",
+                  "2014-01-20", "2014-02-17", "2014-04-18", "2014-05-26",
+                  "2014-07-04", "2014-09-11", "2014-11-27", "2014-12-25",
+                  "2015-01-19", "2015-02-16", "2015-05-25", "2015-07-03",
+                  "2015-09-07", "2015-11-26")
+closed.dates <- as.Date(closed.dates)
+
+# Create first day of the week data file for use on trade entry
+
+open.first.day.week <- data.frame(mon = c(1:12, 1:12, 1:12, 1:12, 1:12, 1:12, 1:12,
+                                     1:12, 1:12, 1:12, 1:12),
+                              year = c(rep(2010, 12),rep(2011, 12), rep(2012, 12),
+                                       rep(2013, 12), rep(2014, 12), rep(2015, 12),
+                                       rep(2016, 12), rep(2017, 12), rep(2018, 12),
+                                       rep(2019, 12), rep(2020, 12)))
+first.monday <- sapply(1:120,
+                       function(i) format(getNthDayOfWeek(first, Mon,
+                                                          open.first.day.week[i,1],
+                                                          open.first.day.week[i,2])))
+second.monday <- sapply(1:120,
+                        function(i) format(getNthDayOfWeek(second, Mon,
+                                           open.first.day.week[i,1],
+                                           open.first.day.week[i,2])))
+third.monday <- sapply(1:120,
+                        function(i) format(getNthDayOfWeek(third, Mon,
+                                                           open.first.day.week[i,1],
+                                                           open.first.day.week[i,2])))
+fourth.monday <- sapply(1:120,
+                        function(i) format(getNthDayOfWeek(fourth, Mon,
+                                                           open.first.day.week[i,1],
+                                                           open.first.day.week[i,2])))
+fifth.monday <- sapply(1:120,
+                        function(i) format(getNthDayOfWeek(fifth, Mon,
+                                                           open.first.day.week[i,1],
+                                                           open.first.day.week[i,2])))
+
+open.first.day.week <- cbind(first.monday, second.monday, third.monday, fourth.monday,
+                        fifth.monday)
+open.first.day.week <- as.data.frame(open.first.day.week, stringsAsFactors = FALSE)
+open.first.day.week <- mutate(open.first.day.week,
+                         fifth.monday = ifelse(fifth.monday == fourth.monday,
+                                               "", fifth.monday))
+
+open.first.day.week <- unlist(open.first.day.week)
+open.first.day.week <- as.data.frame(open.first.day.week)
+names(open.first.day.week) <- "date"
+open.first.day.week <- mutate(open.first.day.week, date = as.Date(date))
+open.first.day.week <- arrange(open.first.day.week, date)
+
+# If date is a market closed date move to next day
+open.first.day.week <- dplyr::mutate(open.first.day.week,
+                                 date = ifelse(date %in% closed.dates,
+                                               date + 1, date))
+
+# Find day of week for each date
+open.first.day.week <- dplyr::mutate(open.first.day.week,
+                                 day.week = weekdays(open.first.day.week$date,
+                                                     abbreviate = FALSE))
+# If date is Sat or Sun move to Monday
+open.first.day.week <- dplyr::mutate(open.first.day.week,
+                                 date = ifelse(day.week == "Saturday",
+                                               date + 2, date))
+open.first.day.week <- dplyr::mutate(open.first.day.week,
+                                 date = ifelse(day.week == "Sunday",
+                                               date + 1, date))
+open.first.day.week <- dplyr::select(open.first.day.week, date)
+open.first.day.week <- dplyr::mutate(open.first.day.week,
+                                 date = as.Date(date, origin = "1970-01-01"))
+
+# Remove NA values
+open.first.day.week <- as.data.frame(open.first.day.week[complete.cases(open.first.day.week),])
+
+names(open.first.day.week) <- "date"
+
+# Save data to be resused in options.data package
+save(open.first.day.week, file = "data/open.first.day.week.RData")
+
+
+
